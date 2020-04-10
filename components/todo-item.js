c6b4eb06d45e2d2eda0c78c0683798bbaa2b92da
EunKo <koeun0529@gmail.com>
2020-04-10 15:24:04 +0900

    commit todo-item

--- components/todo-item.js
+++ components/todo-item.js
@@ -1,11 +1,57 @@
-import React from "react";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import styled from 'styled-components';
 
-export default function TodoItem() {
-  return <Wrapper>Todo</Wrapper>;
+export default class TodoItem extends Component {
+	handleRemove = () => {
+		const { todolist, onRemove } = this.props;
+		onRemove(todolist.id);
+	};
+
+	render() {
+		const { date, todo } = this.props.todolist;
+
+		return (
+			<TodoItemWrapper>
+				<Todo>{todo}</Todo>
+				<Date>
+					{date}
+					<Button onClick={this.handleRemove}>완료</Button>
+				</Date>
+			</TodoItemWrapper>
+		);
+	}
 }
+const Date = styled.div`
+	font-size: 1.5rem;
+	display: flex;
+	color: white;
+	flex-direction: column;
+`;
+const Todo = styled.div`
+	font-size: 1.5rem;
+	color: white;
+	display: flex;
+	flex-direction: column;
+`;
+const Button = styled.button`
+	color: white;
+	background-color: rgb(97, 97, 97);
+	font-size: 1.5rem;
+	outline: none;
+	border-width: initial;
+	border-style: none;
+	padding: 0.5rem 1rem;
+	border-radius: 0.3rem;
+`;
+const TodoItemWrapper = styled.div`
+	font-size: 18px;
+	border: solid 1px black;
 
-const Wrapper = styled.div`
-  font-size: 18px;
-  border: solid 1px;
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: flex-start;
+	margin-bottom: 3rem;
+	padding: 1rem 2rem;
 `;
