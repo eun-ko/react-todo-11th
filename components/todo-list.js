bf2bf80d266165627f834386d0f952dbae8b3f5f
EunKo <koeun0529@gmail.com>
2020-04-11 23:58:21 +0900

    todo-list.js 수정

--- components/todo-list.js
+++ components/todo-list.js
@@ -4,19 +4,26 @@ import styled from 'styled-components';
 import TodoItem from './todo-item';
 
 export default function TodoList(props) {
-	const { data, onRemove } = props;
+	const { todos, onDelete } = props;
 
-	const list = data
-		.sort((todo1, todo2) => {
-			return todo1.date - todo2.date;
-		})
-		.map((todolist) => (
-			<TodoItem key={todolist.id} {...todolist} onRemove={onRemove} />
-		));
-	return <TodoListWrapper> {list} </TodoListWrapper>;
+	return (
+		<Wrapper>
+			{todos
+				.sort((todo1, todo2) => {
+					return todo1.date - todo2.date;
+				})
+				.map((todoObj, index) => (
+					<TodoItem
+						key={JSON.stringify(todo)}
+						{...{ todoObj }}
+						onDelete={onDelete(index)}
+					/>
+				))}
+		</Wrapper>
+	);
 }
 
-const TodoListWrapper = styled.div`
+const Wrapper = styled.div`
 	font-size: 18px;
 	flex: 1;
 `;
