6a43e90c1489c74cc616f1569637c621b86d6071
EunKo <koeun0529@gmail.com>
2020-04-11 15:41:34 +0900

    modified index into functionform

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import TodoInput from '../components/todo-input';
 import TodoList from '../components/todo-list';
 
-export default class Home extends Component {
-	id = 0;
-	state = {
-		todos: [],
+export default function Home() {
+	const [date, setDate] = useState();
+	const [todo, setTodo] = useState();
+	const [todos, setTodos] = useState([]);
+	const [id, setID] = useState(0);
+
+	const changeTodo = (e) => {
+		setTodo(e.target.value);
 	};
 
-	handleCreate = (data) => {
-		const { todos } = this.state;
-		this.setState({
-			todos: todos.concat({ id: this.id++, ...data }),
-		});
-	};
-	handleRemove = (id) => {
-		const { todos } = this.state;
-		this.setState({
-			todos: todos.filter((info) => info.id !== id),
-		});
+	const changeDate = (e) => {
+		setDate(e.target.value);
 	};
 
-	render() {
-		const { todos } = this.state;
+	const removeTodo = (id) => {
+		const newTodos = [...todos];
+		setTodos(newTodos.filter((info) => info.id !== id));
+	};
+	const submitTodo = (e) => {
+		e.preventDefault();
+		if (todo === '' || date === '') {
+			alert('모든 항목을 입력해주세요!');
+			return false;
+		} else if (date.length != 8) {
+			alert('날짜를 올바른 형식으로 입력해주세요!');
+			return false;
+		} else alert('입력되었습니다!');
+		setTodos(todos.concat({ id: id, date: date, todo: todo }));
+		setTodo('');
+		setDate('');
+		setID(id + 1);
+	};
 
-		return (
-			<Wrapper>
-				<Subject>리액트-투두</Subject>
-				<Contents>
-					<TodoInput onCreate={this.handleCreate}></TodoInput>
-					<Space />
-					<TodoList data={todos} onRemove={this.handleRemove} />
-				</Contents>
-			</Wrapper>
-		);
-	}
+	return (
+		<Wrapper>
+			<Subject>리액트-투두</Subject>
+			<Contents>
+				<TodoInput
+					date={date}
+					todo={todo}
+					onChangeDate={changeDate}
+					onChangeTodo={changeTodo}
+					onSubmit={submitTodo}
+				/>
+				<Space />
+				<TodoList data={todos} onRemove={removeTodo} />
+			</Contents>
+		</Wrapper>
+	);
 }
 const Space = styled.div`
 	flex: 0.3;
@@ -57,7 +73,6 @@ const Subject = styled.div`
 `;
 const Contents = styled.div`
 	font-size: 0.1rem;
-
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
