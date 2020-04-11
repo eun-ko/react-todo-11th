36ca813874e6bc87a66b4b37d7bb44f1cc9d78de
EunKo <koeun0529@gmail.com>
2020-04-11 15:41:04 +0900

    modified todo-list into Functionform

--- components/todo-list.js
+++ components/todo-list.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import TodoItem from './todo-item';
 
-export default class TodoList extends Component {
-	render() {
-		const { data, onRemove } = this.props;
-		const list = data.map((todolist) => (
-			<TodoItem key={todolist.id} todolist={todolist} onRemove={onRemove} />
+export default function TodoList(props) {
+	const { data, onRemove } = props;
+
+	const list = data
+		.sort((todo1, todo2) => {
+			return todo1.date - todo2.date;
+		})
+		.map((todolist) => (
+			<TodoItem key={todolist.id} {...todolist} onRemove={onRemove} />
 		));
-		return <TodoListWrapper> {list} </TodoListWrapper>;
-	}
+	return <TodoListWrapper> {list} </TodoListWrapper>;
 }
 
 const TodoListWrapper = styled.div`
